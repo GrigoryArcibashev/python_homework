6d3971c29a7c7ba7ecc5aaf2ff05bd1a45c29b0c
arcibashev71 <arcibashev71@yandex.ru>
2021-10-17 00:04:12 +0500

    (HW3) НЕ храним контент Используем компилирование id:1686043784

--- homework3/phil.py
+++ homework3/phil.py
@@ -7,7 +7,6 @@ from urllib.error import URLError, HTTPError
 
 BASE_URL = 'http://ru.wikipedia.org/wiki/'
 _pattern_for_links = re.compile(r'/wiki/([^#:.]+?)["\']', re.DOTALL)
-_content_of_articles = dict()
 
 
 def get_content(name: str):
@@ -74,11 +73,7 @@ def find_chain(start: str, finish: str):
 
 
 def get_links_in_article(article: str):
-    try:
-        content = _content_of_articles[article]
-    except KeyError:
-        content = get_content(article)
-        _content_of_articles[article] = content
+    content = get_content(article)
     if content is None:
         return None
     start_of_content, end_of_content = extract_content(content)
