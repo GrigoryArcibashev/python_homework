20316fb957239720dca8347b7d17308050a3bb80
arcibashev71 <arcibashev71@yandex.ru>
2021-10-23 15:29:01 +0500

    (HW4) DONE

--- homework4/utility.py
+++ homework4/utility.py
@@ -1,7 +1,6 @@
-import sys
 import os
 import os.path
-from pathlib import Path
+import sys
 from enum import Enum
 
 MODES = Enum('MODES', 'POPRES POPUSER HELP')
@@ -47,10 +46,11 @@ def report_error(error_message: str, error_code: int):
     """
     Информирует об оишбке, после чего аварийно завершает выполнение программы
     """
-    print('%s.\n' % error_message +
-          'Подробнее об использовании утилиты ' +
-          'можно узнать с помощью команды help:\n' +
-          'python %s help' % sys.argv[0])
+    print(
+        '%s.\n' % error_message +
+        'Подробнее об использовании утилиты ' +
+        'можно узнать с помощью команды help:\n' +
+        'python %s help' % sys.argv[0])
     exit(error_code)
 
 
@@ -65,44 +65,70 @@ def get_mode_by_name(mode_name: str):
     return None
 
 
-def get_lines_from_file(file_path: str, start_byte: int, max_bytes: int):
-    with open(file_path, 'r') as file:
+def read_lines(file_path: str, start_byte: int, max_bytes: int):
+    """
+    Возвращает список строк из файла и указатель на следующий
+    непрочитанный байт в файле. Первая строка начинается
+    с позиции start_byte. Будет возвращено минимальное количество строк,
+    чья суммарная длина состовялет не меньше max_bytes.
+    """
+    with open(file_path, 'r', encoding='cp1251') as file:
         file.seek(start_byte)
-        start_outside_file = not bool(file.read(1))
-        if start_outside_file:
-            return None
-        file.seek(file.tell() - 1)
-        t = file.tell()
-        while t > 0 and file.read(1) != '\n':
-            file.seek(t - 2)
-            t = file.tell()
-        if file.read(1) != '\n':
-            file.seek(file.tell() - 1)
-        t = file.tell()
-        read = file.read(max_bytes)
-        if '\n' in read or len(read) <= max_bytes:
-            lines = read.split('\n')
-        else:
-            lines = ''
-        lines = list(filter(lambda line: len(line) > 0, lines))
-        if len(lines) > 0 \
-                and lines[-1][-1] != '\n' \
-                and not len(read) <= max_bytes:
-            # file.seek(file.tell() - len(lines[-1]) + 1)
-            lines = lines[:-1]
-        return list(
-            map(lambda line: line.strip(), lines)), start_byte + max_bytes
+        lines = list()
+        number_of_bytes_read = 0
+        line = file.readline()
+        while line and number_of_bytes_read < max_bytes:
+            lines.append(line)
+            number_of_bytes_read += len(line)
+            line = file.readline()
+        if lines:
+            return lines, start_byte + number_of_bytes_read
+        return None, start_byte
 
 
-def find_most_popular_resource():
-    return None
+def find_most_popular_resource(file_path: str):
+    """
+    Возвращает самый популярный ресурс.
+    Если найти такой не получилось, возвращает None.
+    """
+    return get_max_of_column_from_file(file_path, -2)
 
 
-def find_most_active_user():
-    return None
+def find_most_active_user(file_path: str):
+    """
+    Возвращает самого активного клиента.
+    Если найти такого не получилось, возвращает None.
+    """
+    return get_max_of_column_from_file(file_path, 0)
+
+
+def get_max_of_column_from_file(file_path: str, column_number: int):
+    """
+    Возвращает максимальный элемент из столбца в указнном файле лога.
+    Если найти такой не получилось, возвращает None.
+    """
+    stat = dict()
+    buffer_size = 10 * 1024 * 1024
+    lines, start_byte = read_lines(file_path, 0, buffer_size)
+    while lines is not None:
+        for line in lines:
+            columns = list(
+                filter(
+                    lambda l: len(l) > 0,
+                    map(lambda l: l.strip(), line.split(','))))
+            if len(columns) == 15:
+                try:
+                    stat[columns[column_number]] += 1
+                except KeyError:
+                    stat[columns[column_number]] = 1
+        lines, start_byte = read_lines(file_path, start_byte, buffer_size)
+    return max(stat, key=lambda k: stat[k]) if stat else None
 
 
 def get_help():
+    """
+    Возвращает помощь по использованию утилиты
+    """
     return """
     Утилита, которая по данному логу, согласно параметрам, выдаёт одну 
     из статистик: самый популярный ресурс и самый активный клиент.
@@ -122,19 +148,14 @@ def get_help():
 def main():
     file_path, mode = get_arguments()
     if mode is MODES.POPRES:
-        result = find_most_popular_resource()
+        result = find_most_popular_resource(file_path)
         print(result if result is not None else 'Не удалось выполнить команду')
     elif mode is MODES.POPUSER:
-        result = find_most_active_user()
+        result = find_most_active_user(file_path)
         print(result if result is not None else 'Не удалось выполнить команду')
     else:
         print(get_help())
 
 
 if __name__ == '__main__':
-    # main()
-    c = 0
-    lns, c = get_lines_from_file('t.txt', c, 15)
-    while lns is not None:
-        lns, c = get_lines_from_file('t.txt', c, 15)
-        print(lns)
+    main()
